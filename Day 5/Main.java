359ab07fb5ef5450156ec9e821a72adfcb57ae91
venkatesaperumal87 <139420406+venkatesaperumal87@users.noreply.github.com>
2023-07-14 19:21:54 +0530

    Add files via upload

---
+++ Day 5/Main.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+public int isMultiple(int input1,int input2){ 
+
+if(input1==0 || input2==0) 
+
+   return 3; 
+
+else if(input1%input2==0)   
+
+   return 2; 
+
+else 
+
+   return 1;
+
+}}
